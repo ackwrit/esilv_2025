8bbbe556bc6f586bc3e809da7399a5b4670aaf2a
skweel <dissingardjino@icloud.com>
2025-01-17 09:49:16 +0100

    mise à jour

--- lib/view/mydashboard.dart
+++ lib/view/mydashboard.dart
@@ -1,4 +1,5 @@
 import 'package:first_app_2025_esilv/controller/global.dart';
+import 'package:first_app_2025_esilv/controller/myFiresbaseHelper.dart';
 import 'package:first_app_2025_esilv/view/myBackground.dart';
 import 'package:flutter/material.dart';
 
@@ -10,6 +11,9 @@ class MyDasgBoard extends StatefulWidget {
 }
 
 class _MyDasgBoardState extends State<MyDasgBoard> {
+  //variable
+  bool isChangePseudo = false;
+  TextEditingController pseudoController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,6 +21,51 @@ class _MyDasgBoardState extends State<MyDasgBoard> {
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width * 0.33,
         color: Colors.amber,
+        child: Column(children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: NetworkImage(moi.avatar!), fit: BoxFit.fill),
+            ),
+          ),
+          SizedBox(height: 10),
+          Text(moi.email),
+          SizedBox(height: 10),
+          Text(moi.prenom),
+          SizedBox(height: 10),
+          Text(moi.nom),
+          SizedBox(height: 10),
+          Row(children: [
+            (isChangePseudo)
+                ? Container(
+                    padding: EdgeInsets.all(5),
+                    width: MediaQuery.of(context).size.width * 0.2,
+                    child: TextField(
+                        controller: pseudoController,
+                        decoration:
+                            InputDecoration(hintText: "Entrer le pseudo")))
+                : Text(moi.pseudo!),
+            Spacer(),
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  isChangePseudo = true;
+                  if (isChangePseudo == true) {
+                    setState(() {
+                      moi.pseudo = pseudoController.text;
+                      Map<String, dynamic> data = {"PSEUDO": moi.pseudo};
+                      MyFirebaseHelper().updateUser(moi.id, data);
+                      isChangePseudo = false;
+                    });
+                  }
+                });
+              },
+              icon: Icon(Icons.check),
+            ),
+          ])
+        ]),
       ),
       appBar: AppBar(
         backgroundColor: Colors.transparent,
